06f1800783ad95d59a8ac81b80412c056777d656
timeschanged <117090547+timeschanged@users.noreply.github.com>
2023-04-17 16:30:41 +0530

    Time: 4 ms (36.22%), Space: 6.5 MB (49.53%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    vector<string> ans;
-    map<int, string> mpp;
-    void f(int i, string s,const string &dig) {
-        int n = dig.size();
-        if(i == n) {
-            ans.push_back(s);
-            return;
-        }
-        int nos = (dig[i]-'0');
-        for(auto ch: mpp[nos]) {
-            s.push_back(ch);
-            f(i+1, s, dig);
-            s.pop_back();
-        }
-        return;
-    }
+    const vector<string> pad = {
+        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
     vector<string> letterCombinations(string digits) {
-        if(digits.size() == 0) return ans;
-        mpp[2] = "abc", mpp[3] = "def", mpp[4] = "ghi", mpp[5] = "jkl", mpp[6] = "mno", mpp[7] = "pqrs", mpp[8] = "tuv", mpp[9] = "wxyz";
-        f(0, "", digits);
+        int n = digits.size();
+        if(n == 0) return {};
+        vector<string> ans;
+        ans.push_back("");
+        for(auto &dig: digits){
+            vector<string> temp;
+            for(auto &str: pad[dig-'0']) {
+                for(auto &s: ans) {
+                    temp.push_back(s + str);
+                }
+            }
+            ans.swap(temp);
+        }
         return ans;
     }
 };
